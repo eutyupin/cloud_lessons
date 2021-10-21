f02a95dc4d8c83474ebf1094e70cb8877961eb02
eutyupin <eu-1982@yandex.ru>
2021-10-21 23:37:26 +0700

    lesson1

---
+++ cloud_lessons_client/src/main/java/ru/gb/nio/NioServer.java
@@ -0,0 +1,76 @@
+package ru.gb.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
+import java.util.Set;
+
+public class NioServer {
+
+    private ServerSocketChannel server;
+    private Selector selector;
+    private ByteBuffer buffer;
+
+    public NioServer() throws Exception {
+        buffer = ByteBuffer.allocate(256);
+        server = ServerSocketChannel.open();
+        server.bind(new InetSocketAddress(8189));
+        selector = Selector.open();
+        server.configureBlocking(false);
+        server.register(selector, SelectionKey.OP_ACCEPT);
+        while (server.isOpen()) {
+            selector.select();
+            Set<SelectionKey> selectedKeys = selector.selectedKeys();
+            Iterator<SelectionKey> iterator = selectedKeys.iterator();
+            while (iterator.hasNext()) {
+                SelectionKey key = iterator.next();
+                if (key.isAcceptable()){
+                    handleAccept(key);
+                }
+                if (key.isReadable()) {
+                    handleRead(key);
+                }
+                iterator.remove();
+            }
+        }
+    }
+
+    private void handleRead(SelectionKey key) throws IOException {
+        SocketChannel chanel = (SocketChannel) key.channel();
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            int read = chanel.read(buffer);
+            if (read == -1) {
+                chanel.close();
+                return;
+            }
+            if (read == 0) {
+                break;
+            }
+            buffer.flip();
+            while (buffer.hasRemaining()) {
+                sb.append((char) buffer.get());
+            }
+            buffer.clear();
+        }
+        String result = "[From server]: " + sb.toString();
+        chanel.write(ByteBuffer.wrap(result.getBytes(StandardCharsets.UTF_8)));
+    }
+
+    private void handleAccept(SelectionKey key) throws Exception{
+        ServerSocketChannel server = (ServerSocketChannel) key.channel();
+        SocketChannel channel = server.accept();
+        channel.register(selector, SelectionKey.OP_READ, "Hello world");
+    }
+
+    public static void main(String[] args) throws Exception {
+        new NioServer();
+    }
+}
---
+++ cloud_lessons_client/src/main/java/ru/gb/nio/NioUtils.java
@@ -0,0 +1,6 @@
+package ru.gb.nio;
+
+public class NioUtils {
+
+
+}
